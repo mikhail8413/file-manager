86a63d65ef099125758971ee180027b76b85c9c3
mikhail8413 <62240259+mikhail8413@users.noreply.github.com>
2020-07-02 03:16:52 +0300

    Add files via upload

---
+++ Файловый менеджер/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace файловый_менеджер
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.убитьWindowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(23, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(299, 310);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(253, 67);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(69, 23);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Перейти";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(23, 96);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(44, 13);
+            this.metroButton2.TabIndex = 4;
+            this.metroButton2.Text = "<";
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Tray";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.закрытьToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.убитьWindowsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(162, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // убитьWindowsToolStripMenuItem
+            // 
+            this.убитьWindowsToolStripMenuItem.Name = "убитьWindowsToolStripMenuItem";
+            this.убитьWindowsToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.убитьWindowsToolStripMenuItem.Text = "Убить Windows ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 435);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.listBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "Rodionov Mikhail";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem убитьWindowsToolStripMenuItem;
+    }
+}
+
---
+++ Файловый менеджер/Form1.cs
@@ -0,0 +1,152 @@
+﻿using MetroFramework.Components; //Делаем всеми любимый графений
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics; // подключаем библиотеку для открытия файлов
+using System.IO; //подключаем библиотеку для юзания в дисковом пространстве
+
+namespace файловый_менеджер
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MetroStyleManager metroStyleManager1 = null; //Оживляем графений, если вылезает ошибка удалить и вставить за ного эти две строки
+            this.StyleManager = metroStyleManager1;
+            notifyIcon1.BalloonTipTitle = "File Menedger Power by AnRub"; // отображение для уведоблений
+            notifyIcon1.BalloonTipText = "File Menedger has been worked..."; // отображение для уведоблений
+            notifyIcon1.Text = "Virus Detected"; // отображение для уведоблений
+        }
+
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); // фикс бага с наслойкой директорий друг на друга
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text); // передаем путь в конструктор
+            DirectoryInfo[] dirs = dir.GetDirectories(); // Масив для хранения катологов
+            foreach (DirectoryInfo ssDir in dirs) // перебор элементов масива
+            {
+                listBox1.Items.Add(ssDir); // добовляем в список элемент
+            }
+
+            FileInfo[] files = dir.GetFiles(); // отображаем сами файлы
+            foreach (FileInfo ssFile in files) // выводим все содержащиеся файлы на экран
+            {
+                listBox1.Items.Add(ssFile); // вывод
+            }
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e) // пользовательский клик
+        {
+            if (Path.GetExtension(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())) == "") //открытие файла
+            {
+                textBox1.Text = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString()); //выебнулся
+                listBox1.Items.Clear(); // фикс бага с наслойкой директорий друг на друга
+                DirectoryInfo dir = new DirectoryInfo(textBox1.Text); // передаем путь в конструктор
+                DirectoryInfo[] dirs = dir.GetDirectories(); // Масив для хранения катологов
+                foreach (DirectoryInfo ssDir in dirs) // перебор элементов масива
+                {
+                    listBox1.Items.Add(ssDir); // добовляем в список элемент
+                }
+
+                FileInfo[] files = dir.GetFiles(); // отображаем сами файлы
+                foreach (FileInfo ssFile in files) // выводим все содержащиеся файлы на экран
+                {
+                    listBox1.Items.Add(ssFile); // вывод
+                }
+            }
+    else
+            {
+                Process.Start(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())); // Дефолтный вывод
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e) // описываем кнопку назад
+        {
+            if (textBox1.Text[textBox1.Text.Length - 1] == '\\') // стриаем сеши
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1); // удаляем один символ
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+            else if (textBox1.Text[textBox1.Text.Length - 1 ] != '\\' )
+            {
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+            listBox1.Items.Clear(); // фикс бага с наслойкой директорий друг на друга
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text); // передаем путь в конструктор
+            DirectoryInfo[] dirs = dir.GetDirectories(); // Масив для хранения катологов
+            foreach (DirectoryInfo ssDir in dirs) // перебор элементов масива
+            {
+                listBox1.Items.Add(ssDir); // добовляем в список элемент
+            }
+
+            FileInfo[] files = dir.GetFiles(); // отображаем сами файлы
+            foreach (FileInfo ssFile in files) // выводим все содержащиеся файлы на экран
+            {
+                listBox1.Items.Add(ssFile); // вывод
+            }
+
+        }
+
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) // Добавляем видимость отрицательную при открытом окне проги
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e) // добавляем положительную видимость на 1 сек. или ее отсутсвие в скрытом состоянии. 
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close(); // зарывает окно
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();  // открывает окно
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
